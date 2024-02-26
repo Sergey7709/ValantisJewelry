c8147431ebd35b41c7f5ba9949b597e112dc3932
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-26 13:30:41 +0300

    feat: textField component

---
+++ src/components/ui/textField/textField.tsx
@@ -0,0 +1,114 @@
+import { ComponentProps, ComponentPropsWithoutRef, forwardRef, useState } from 'react'
+
+import { Close } from '@/assets/icons/close'
+import Eye from '@/assets/icons/eye'
+import { Search } from '@/assets/icons/search'
+import VisibilityOff from '@/assets/icons/visibilityOff'
+import { Typography } from '@/components/ui/typography'
+import { clsx } from 'clsx'
+
+import s from './textField.module.scss'
+
+export type TextFieldProps = {
+  containerProps?: ComponentProps<'div'>
+  errorMessage?: string
+  label?: string
+  labelProps?: ComponentProps<'label'>
+  onValueChange?: (value: string) => void
+  type?: 'password' | 'search' | 'text'
+} & ComponentPropsWithoutRef<'input'>
+
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  (
+    {
+      className,
+      containerProps,
+      errorMessage,
+      label,
+      labelProps,
+      onChange,
+      onValueChange,
+      placeholder,
+      type,
+      value,
+      ...restProps
+    },
+    ref
+  ) => {
+    const [showPassword, setShowPassword] = useState(false)
+
+    const [localValue, setLocalValue] = useState<string>('')
+
+    const inputValue = value ?? localValue
+
+    const isShowPasswordButtonShown = type === 'password'
+    const isSearchInput = type === 'search'
+
+    const finalType = getFinalType(type, showPassword)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setLocalValue(e.target.value)
+      onChange?.(e)
+      onValueChange?.(e.target.value)
+    }
+
+    const clearHandler = () => {
+      setLocalValue('')
+      onValueChange?.('')
+    }
+
+    const classNames = {
+      error: clsx(s.error),
+      field: clsx(s.field, !!errorMessage && s.error, className),
+      fieldContainer: clsx(s.fieldContainer),
+      label: clsx(s.label, labelProps?.className),
+      root: clsx(s.root, containerProps?.className),
+    }
+
+    return (
+      <div className={classNames.root}>
+        {label && (
+          <Typography as={'label'} className={classNames.label} variant={'body2'}>
+            {label}
+          </Typography>
+        )}
+        <div className={classNames.fieldContainer}>
+          {isSearchInput && <Search className={s.searchIcon} />}
+          <input
+            className={classNames.field}
+            onChange={handleChange}
+            placeholder={placeholder}
+            ref={ref}
+            type={finalType}
+            value={inputValue}
+            {...restProps}
+          />
+
+          {inputValue && isSearchInput && <Close className={s.closeIcon} onClick={clearHandler} />}
+
+          {isShowPasswordButtonShown && (
+            <button
+              className={s.showPassword}
+              onClick={() => setShowPassword(prev => !prev)}
+              type={'button'}
+            >
+              {showPassword ? <VisibilityOff /> : <Eye />}
+            </button>
+          )}
+        </div>
+
+        <Typography className={classNames.error} variant={'error'}>
+          {errorMessage}
+        </Typography>
+      </div>
+    )
+  }
+)
+
+function getFinalType(type: ComponentProps<'input'>['type'], showPassword: boolean) {
+  if (type === 'password' && showPassword) {
+    return 'text'
+  }
+
+  return type
+}
