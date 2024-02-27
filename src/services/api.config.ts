c3f0f54ee0cc5e52ec55ac2af255afb232940e7b
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-27 18:13:47 +0300

    refactor: edit value baseURL and 'X-Auth'

--- src/services/api.config.ts
+++ src/services/api.config.ts
@@ -3,10 +3,10 @@ import axios from 'axios'
 import md5 from 'md5'
 
 export const instance = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    'X-Auth': md5(`${process.env.AUTH_VALUE_PART}${getTimestamp()}`),
+    'X-Auth': md5(`${import.meta.env.VITE_AUTH_VALUE_PART}${getTimestamp()}`),
   },
 })
