dd5f87ece3b9a14d1d671f322c49d1f84094c4ce
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-27 11:22:14 +0300

    feat: add interface ImportMetaEnv  and ImportMeta

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly API_BASE_URL: string;
+  readonly AUTH_VALUE_PART: string;
+
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
