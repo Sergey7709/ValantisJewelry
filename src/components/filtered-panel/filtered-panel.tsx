ce297662224a5ef529d95e78fdbc68b819588a16
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-28 22:31:17 +0300

    refactor: add type FilteredPanelProps and prop onHandleSubmitForm

--- src/components/filtered-panel/filtered-panel.tsx
+++ src/components/filtered-panel/filtered-panel.tsx
@@ -1,5 +1,19 @@
-// import s from './filtered-panel.module.scss'
+import { TextField } from '@/components/ui/text-field'
+import { FilterParams } from '@/pages/dashboard/types.dashboard'
 
-export const FilteredPanel = () => {
-  return <div>Filter</div>
+import s from './filtered-panel.module.scss'
+
+type FilteredPanelProps = {
+  onHandleSubmitForm: (form: FilterParams) => void
+}
+export const FilteredPanel = ({ onHandleSubmitForm }: FilteredPanelProps) => {
+  return (
+    <div>
+      <form>
+        <div className={s.filterTextField}>
+          <TextField label={'search'} type={'search'} />
+        </div>
+      </form>
+    </div>
+  )
 }
