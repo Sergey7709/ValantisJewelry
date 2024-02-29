b07f9bc0f97ba234905b8f6d941b4cae7405f4bf
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-29 18:35:26 +0300

    feat: add variables: searchValueTextField and searchValueSelect in useState. Add useEffect for submit params. Add  handlerChangeValueTextField and handlerSelect.

--- src/components/filtered-panel/filtered-panel.tsx
+++ src/components/filtered-panel/filtered-panel.tsx
@@ -1,29 +1,61 @@
+import { useEffect, useState } from 'react'
+
 import { Select } from '@/components/ui/select'
 import { TextField } from '@/components/ui/text-field'
-import { FilterParams } from '@/pages/dashboard/types.dashboard'
+import { FilterParams, KeysFilterParams } from '@/pages/dashboard/types.dashboard'
+import { useDebounce } from '@/shared/hooks'
 
 import s from './filtered-panel.module.scss'
 
 type FilteredPanelProps = {
-  onHandleSubmitForm: (form: FilterParams) => void
+  onHandleSubmitParams: (form: FilterParams) => void
 }
+
+type SearchValue = string
 
 const optionsSelect = ['brand', 'price', 'product']
 
-export const FilteredPanel = ({ onHandleSubmitForm }: FilteredPanelProps) => {
+const milliSecondsValue = 1000
+
+export const FilteredPanel = ({ onHandleSubmitParams }: FilteredPanelProps) => {
+  const [searchValueTextField, setSearchValueTextField] = useState<SearchValue>('')
+  const [searchValueSelect, setSearchValueSelect] = useState<KeysFilterParams>('product')
+
+  const debounce = useDebounce({ milliSeconds: milliSecondsValue, value: searchValueTextField })
+
+  useEffect(() => {
+    if (debounce.length >= 1) {
+      const submitValueSelect = {
+        [searchValueSelect]:
+          searchValueSelect === 'price'
+            ? Number(searchValueTextField.toString())
+            : searchValueTextField,
+      } as FilterParams
+
+      onHandleSubmitParams(submitValueSelect)
+    }
+  }, [debounce])
+
+  const handlerChangeValueTextField = (value: SearchValue) => {
+    setSearchValueTextField(value)
+  }
+  const handlerSelect = (newValue: KeysFilterParams) => {
+    setSearchValueSelect(newValue)
+  }
+
   return (
     <div>
-      <form>
-        <div className={s.filterTextField}>
-          <Select
-            onValueChange={() => {}}
-            options={optionsSelect}
-            placeholder={'Select the filter type'}
-            variant={'common'}
-          />
-          <TextField label={'Enter a value for filtering'} type={'search'} />
-        </div>
-      </form>
+      <div className={s.filterTextField}>
+        <Select
+          label={'Filter'}
+          onValueChange={handlerSelect}
+          options={optionsSelect}
+          placeholder={'Select the filter type'}
+          value={searchValueSelect}
+          variant={'common'}
+        />
+        <TextField label={'Search'} onValueChange={handlerChangeValueTextField} type={'search'} />
+      </div>
     </div>
   )
 }
