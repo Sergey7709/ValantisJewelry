c61efd331a8d54db10eeb2257a5d65dd15b7fc94
Sergey Koshelev <shivacharya2019@gmail.com>
2024-02-29 11:18:52 +0300

    feat: add variable optionsSelect and component Select.

--- src/components/filtered-panel/filtered-panel.tsx
+++ src/components/filtered-panel/filtered-panel.tsx
@@ -1,3 +1,4 @@
+import { Select } from '@/components/ui/select'
 import { TextField } from '@/components/ui/text-field'
 import { FilterParams } from '@/pages/dashboard/types.dashboard'
 
@@ -6,12 +7,22 @@ import s from './filtered-panel.module.scss'
 type FilteredPanelProps = {
   onHandleSubmitForm: (form: FilterParams) => void
 }
+
+const optionsSelect = ['brand', 'price', 'product']
+
 export const FilteredPanel = ({ onHandleSubmitForm }: FilteredPanelProps) => {
   return (
     <div>
       <form>
         <div className={s.filterTextField}>
-          <TextField label={'search'} type={'search'} />
+          <Select
+            label={'Filter'}
+            onValueChange={() => {}}
+            options={optionsSelect}
+            placeholder={'Select the filter type'}
+            variant={'common'}
+          />
+          <TextField label={'Enter a value for filtering'} type={'search'} />
         </div>
       </form>
     </div>
