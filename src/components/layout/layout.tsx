333b98a0b31c407761c4bf007443d2d8cd2c1469
Sergey Koshelev <shivacharya2019@gmail.com>
2024-03-02 00:30:30 +0300

    refactor: add className

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,11 +1,18 @@
 import { ReactNode } from 'react'
 
 import { ThemeMode } from '@/components/ui/theme-mode'
+import { Typography } from '@/components/ui/typography'
+
+import s from './layout.module.scss'
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-      <ThemeMode />
+    <div className={s.layout_container}>
+      <header className={s.header}>
+        <Typography className={s.title}>Valantis Jewelry</Typography>
+        <ThemeMode />
+      </header>
+
       <div>{children}</div>
     </div>
   )
