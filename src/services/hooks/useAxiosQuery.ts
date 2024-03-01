cbdb233b713b3ad2f21c96ae2f418cb6e54d02f6
Sergey Koshelev <shivacharya2019@gmail.com>
2024-03-01 18:53:22 +0300

    refactor: Added default value for recursion in error handling

--- src/services/hooks/useAxiosQuery.ts
+++ src/services/hooks/useAxiosQuery.ts
@@ -12,39 +12,38 @@ export const useAxiosQuery = <T>({ params }: AxiosQuery) => {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<null | string>(null)
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        setLoading(true)
-        const response: AxiosResponse<T> = await instance('', { ...params })
+  const getData = async (retryCount = 0) => {
+    try {
+      setLoading(true)
+      const response: AxiosResponse<T> = await instance('', { ...params })
 
-        setData(response.data)
-        setLoading(false)
-      } catch (error) {
-        if (isAxiosError(error)) {
-          const Error: AxiosErrorType = {
-            message: error.message,
-          }
-
-          console.error(Error.message)
-          setError(`Error getting the data: ${Error.message}`)
-          setLoading(false)
-        } else {
-          console.error(`Unknown error occurred:${error}`)
-          setError(`Unknown error occurred:${error}`)
-          setLoading(false)
+      setData(response.data)
+      setLoading(false)
+    } catch (error) {
+      if (isAxiosError(error)) {
+        const Error: AxiosErrorType = {
+          message: error.message,
         }
+
+        console.error(Error.message)
+        setError(`Error getting the data: ${Error.message}`)
+        setLoading(false)
+
+        if (retryCount < 2) {
+          console.log('retry')
+          getData(retryCount + 1)
+        }
+      } else {
+        console.error(`Unknown error occurred:${error}`)
+        setError(`Unknown error occurred:${error}`)
+        setLoading(false)
       }
     }
+  }
 
+  useEffect(() => {
     getData()
-  }, [
-    params?.data.params.offset,
-    params?.data.action,
-    params?.data.params.product,
-    params?.data.params.price,
-    params?.data.params.brand,
-  ])
+  }, [])
 
-  return { data, error, loading }
+  return { data, error, getData, loading }
 }
