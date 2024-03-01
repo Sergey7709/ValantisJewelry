4f5db22da4ea1fbeb60b15c6ef66d98ce15029bc
Sergey Koshelev <shivacharya2019@gmail.com>
2024-03-02 01:41:35 +0300

    refactor: edit defaultLimit

--- src/pages/dashboard/constants.dashboard.ts
+++ src/pages/dashboard/constants.dashboard.ts
@@ -1,5 +1,5 @@
 export const magicNumber = 8005
 export const defaultPage = 1
-export const defaultLimit = 5
+export const defaultLimit = 50
 export const defaultOffset = 0
 export const defaultValueParams = { limit: defaultLimit, offset: defaultOffset }
