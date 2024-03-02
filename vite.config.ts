d04ef90f63f8646cfd74305ab49be68916d44e0d
Sergey Koshelev <shivacharya2019@gmail.com>
2024-03-03 01:14:48 +0300

    refactor: delete dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import * as path from 'path'
 
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 
-dotenv.config()
 export default defineConfig({
-  define: {
-    'process.env': process.env,
-  },
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
